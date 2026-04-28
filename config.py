8397a3514ecf51f388a9c53450ad0908bb7f1a6e
unknown <razhildelossantos1@gmail.com>
2026-04-28 11:32:41 +0800

    Remove channel_binding to fix psycopg2 libpq error on Vercel

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'lpg-gaspinas-secret-2024-!@#$')
 
     # Neon PostgreSQL database
-    NEON_DB_URL = 'postgresql://neondb_owner:npg_h5JTl0OzuZYU@ep-winter-lake-anho9gpz-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+    NEON_DB_URL = 'postgresql://neondb_owner:npg_h5JTl0OzuZYU@ep-winter-lake-anho9gpz-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require'
     
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', NEON_DB_URL)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
